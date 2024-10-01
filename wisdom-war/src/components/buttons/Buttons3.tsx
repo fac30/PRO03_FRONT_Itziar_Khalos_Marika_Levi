c6dbd5c4f463fcfed97608820ac2d92750cf6474
MarikaBBB <mbertelli012@gmail.com>
2024-10-01 23:12:52 +0100

    refractoring

--- wisdom-war/src/components/buttons/Buttons3.tsx
+++ wisdom-war/src/components/buttons/Buttons3.tsx
@@ -1,14 +1,24 @@
 // cancel
-
 import React from 'react';
 import ButtonTemplate from './ButtonTemplate';
+
+// const Button3: React.FC = () => {
+//   return (
+//     <ButtonTemplate
+//       text="Cancel"
+//       className=""
+//       onClick={() => console.log('Cancel clicked')}
+//     />
+//   );
+// };
 
 const Button3: React.FC = () => {
   return (
     <ButtonTemplate
-      text="Cancel"
-      className=""
-      onClick={() => console.log('Cancel clicked')}
+      text="Next"
+      variant="third"  // Set the icon style variant
+      iconRight={< ""/>}  // Pass the icon for the right side
+      onClick={() => console.log('Next')}
     />
   );
 };
