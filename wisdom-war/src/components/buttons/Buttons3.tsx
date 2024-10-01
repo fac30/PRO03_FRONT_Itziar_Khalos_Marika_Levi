2b643c5f7d75f92d021fd0ab76020cf9483fa7b7
MarikaBBB <mbertelli012@gmail.com>
2024-10-01 22:57:55 +0100

    created buttons components 1,2,3 and tmeplate

--- wisdom-war/src/components/buttons/Buttons3.tsx
+++ wisdom-war/src/components/buttons/Buttons3.tsx
@@ -1,1 +1,16 @@
 // cancel
+
+import React from 'react';
+import ButtonTemplate from './ButtonTemplate';
+
+const Button3: React.FC = () => {
+  return (
+    <ButtonTemplate
+      text="Cancel"
+      className=""
+      onClick={() => console.log('Cancel clicked')}
+    />
+  );
+};
+
+export default Button3;
