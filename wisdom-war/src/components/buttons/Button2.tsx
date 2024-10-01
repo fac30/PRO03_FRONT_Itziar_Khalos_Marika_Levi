757b2f49b8623f2ef0bb360e2914fb4952cb3ff2
MarikaBBB <mbertelli012@gmail.com>
2024-10-01 23:12:52 +0100

    refractoring

--- wisdom-war/src/components/buttons/Button2.tsx
+++ wisdom-war/src/components/buttons/Button2.tsx
@@ -1,14 +1,23 @@
 // next
-
 import React from 'react';
 import ButtonTemplate from './ButtonTemplate';
+
+// const Button2: React.FC = () => {
+//   return (
+//     <ButtonTemplate
+//       text="Next Question"
+//       className=""
+//       onClick={() => console.log('Next')}
+//     />
+//   );
+// };
 
 const Button2: React.FC = () => {
   return (
     <ButtonTemplate
-      text="Next Question"
-      className=""
-      onClick={() => console.log('Next')}
+      text="Cancel"
+      variant="secondary"  // Set the secondary style variant (underline)
+      onClick={() => console.log('Cancel')}
     />
   );
 };
