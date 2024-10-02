02609cb8c925c821bc3a4cb3e754347e9ef17175
MarikaBBB <mbertelli012@gmail.com>
2024-10-02 11:48:23 +0100

    deleted button1,2,3 in the component folder and refractor the button file

--- wisdom-war/src/App.tsx
+++ wisdom-war/src/App.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Button1 from './components/buttons/Button1';
-import Button2 from './components/buttons/Button2';
-import Button3 from './components/buttons/Buttons3';
+
+import Button from './components/buttons/Button';
 
 function App() {
   return (
     <>
       <h1>Welcome to Wisdom War!</h1>
-      {/* Render buttons */}
-      <Button1 />
-      <Button2 />
-      <Button3 />
+    
     </>
   );
 }
