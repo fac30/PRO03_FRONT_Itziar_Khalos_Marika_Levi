0ae53e612e1a968ca10b133ebb50dbbcdbb3d7f7
MarikaBBB <mbertelli012@gmail.com>
2024-10-02 13:07:07 +0100

    feat: refine button style and commented out the handButtonClick

--- wisdom-war/src/components/buttons/Button.tsx
+++ wisdom-war/src/components/buttons/Button.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({
   type = 'button',
 }) => {
   // Define the button's class names
-  const buttonClass = `bg-black text-white py-2 px-6 rounded-full hover:bg-gray-800 ${className}`;
+  const buttonClass = `bg-black text-white px-4 py-2 rounded-md shadow-md hover:bg-gray-800 focus:outline-none ${className}`;
 
   return (
     <button
