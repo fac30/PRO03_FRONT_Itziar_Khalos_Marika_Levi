bdfa1978e93391aa7001a3b5962c6a4327b07b7b
MarikaBBB <mbertelli012@gmail.com>
2024-10-02 13:07:07 +0100

    feat: refine button style and commented out the handButtonClick

--- wisdom-war/src/App.tsx
+++ wisdom-war/src/App.tsx
@@ -3,19 +3,15 @@ import Navbar from "./components/NavBar";
 
 function App() {
   // event handler for the butotn click
-  const handleButtonClick = () => {
-    alert('Button clicked!');
-  };
+  // const handleButtonClick = () => {
+  //   alert('Button clicked!');
+  // };
 
   return (
     <>
       <h1>Welcome to Wisdom War!</h1>
       <Navbar title="Explore Quizzes" />
-      <Button
-      text="Click me!"
-      className=''
-      onClick={handleButtonClick}
-      />
+
     </>
   );
 }
