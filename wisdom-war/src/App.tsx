e7abd59346e77cf3cf96b8afe7dfbd3c56123e7d
Itziar Cantero <itziar.cantero26@gmail.com>
2024-10-02 15:39:06 +0100

    "Add Card component"

--- wisdom-war/src/App.tsx
+++ wisdom-war/src/App.tsx
@@ -2,11 +2,6 @@ import Navbar from "./components/NavBar";
 import Categories from "./components/sections/Categories";
 
 function App() {
-  // event handler for the butotn click
-  // const handleButtonClick = () => {
-  //   alert('Button clicked!');
-  // };
-
   return (
     <>
       <h1>Welcome to Wisdom War!</h1>
