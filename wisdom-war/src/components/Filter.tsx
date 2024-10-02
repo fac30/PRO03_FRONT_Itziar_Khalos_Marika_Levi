963213a58a63b69c1d33f0fca67544b30a9802bb
MarikaBBB <mbertelli012@gmail.com>
2024-10-02 12:47:08 +0100

    create the sidebar component wip

---
+++ wisdom-war/src/components/Filter.tsx
@@ -0,0 +1,16 @@
+
+
+
+// Create the function Filter (the sidebar)
+function Filter(props: FilterProps) {
+    // inside add the filter logic
+   
+}
+
+// Two list to keep track to the selected categuries and levels (State)
+let selectedCategories = [];
+let selectedLevels = [];
+
+// define the categories and levels
+let categories = ["FAC30", "Music", "Movies", "Art"];
+let levels = ["Easy", "Medium", "Hard"];
