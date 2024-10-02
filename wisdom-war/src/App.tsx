04019823c4c02351d1047c827dea6dad2f8c88e4
rogers7levi <rogers.levi714@gmail.com>
2024-10-02 17:17:50 +0100

    merge conflict resolve

--- wisdom-war/src/App.tsx
+++ wisdom-war/src/App.tsx
@@ -1,11 +1,10 @@
-
-import Button from './components/buttons/Button';
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <h1>Welcome to Wisdom War!</h1>
-    
+      <Navbar title="Explore Quizzes" />
     </>
   );
 }
