5ea8b53f48947410bdabb4019991465dd4197edf
MarikaBBB <mbertelli012@gmail.com>
2024-10-09 16:12:35 +0100

    style resultPage

--- wisdom-war/src/Pages/ResultPage.tsx
+++ wisdom-war/src/Pages/ResultPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/NavBar";
 import Button from "../components/buttons/Button";
 
 type ResultPageProps = {
@@ -9,13 +8,18 @@ type ResultPageProps = {
 
 function ResultPage(props: ResultPageProps) {
   return (
-    <div>
-      <h1>Your Quiz Results</h1>
-      <p>
-        Correct Answers: {props.score} out of {props.totalQuestions}
-      </p>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center text-2xl font-bold mt-20">Your Quiz Result: How well do you know your cohort?</h1>
+      
+      <div className="container flex justify-center mx-auto">
+        {/* Empty container */}
+        <div className="empty-container bg-gray-200 border border-black w-64 h-64 my-10"></div>  
+      </div>
+
+      <p className="text-center text-xl font-normal mt-4">Correct Answers: {props.score} out of {props.totalQuestions}</p>
     </div>
   );
 }
+
 
 export default ResultPage;
