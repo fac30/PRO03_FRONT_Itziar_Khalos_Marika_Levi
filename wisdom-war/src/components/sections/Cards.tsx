4ad9754899eb17e333c5c7d64178f833b97fe1d0
Khalos Moscato <khalos.moscato@icloud.com>
2024-10-09 14:51:30 +0100

    made cards element responsive

--- wisdom-war/src/components/sections/Cards.tsx
+++ wisdom-war/src/components/sections/Cards.tsx
@@ -11,15 +11,21 @@ type CardProps = {
 function Card(props: CardProps) {
   return (
     <>
-      <div className="card p-10 rounded-md my-5 max-w-lg">
-        <h3 className="quiz-name text-2xl font-semibold p-2">
+      <div className="card p-6 md:p-8 lg:p-10 rounded-md my-5 max-w-full md:max-w-md lg:max-w-lg mx-auto shadow-lg">
+        <h3 className="quiz-name text-xl md:text-2xl lg:text-3xl font-semibold p-2">
           {props.quizName}
         </h3>
-        <p className="quiz-description p-2">{props.description}</p>
-        <p className="level text-medium font-semibold p-2">
+        <p className="quiz-description p-2 text-sm md:text-base lg:text-lg">
+          {props.description}
+        </p>
+        <p className="level text-sm md:text-base lg:text-lg font-semibold p-2">
           Level: {props.level}
         </p>
-        <Button text="Play" path={`/quiz/${props.quizId}`} className=""></Button>
+        <Button
+          text="Play"
+          path={`/quiz/${props.quizId}`}
+          className="mt-4 w-full md:w-auto"
+        />
       </div>
     </>
   );
