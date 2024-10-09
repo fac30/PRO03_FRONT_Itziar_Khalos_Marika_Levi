bd343f29372e296221faa2684dc6bad0e2b35283
Khalos Moscato <khalos.moscato@icloud.com>
2024-10-09 16:31:34 +0100

    small changes in the question component where the counter is in the same like as the Q&A block

--- wisdom-war/src/components/quiz/Question.tsx
+++ wisdom-war/src/components/quiz/Question.tsx
@@ -26,32 +26,28 @@ const QuestionComponent = (props: QuestionProps) => {
 
   return (
     <div className="w-full max-w-3xl mx-auto mt-8 p-4 bg-white rounded-lg shadow-sm">
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-start mb-6"> {/* Flex container for alignment */}
+        <h2 className="text-xl mb-4">{question.text}</h2>
         <span className="bg-gray-200 text-sm px-4 py-2 rounded-md">
           {index + 1}/{totalQuestions}
         </span>
       </div>
-      <div className="w-full">
-        <div key={question.id}>
-          <h2 className="text-xl mb-4">{question.text}</h2>
-          <ul className="space-y-2">
-            {question.answers?.map((answer) => (
-              <li key={answer.id} className="ml-4">
-                <label className="flex items-center space-x-3">
-                  <input
-                    type="radio"
-                    name={`question-${question.id}`}
-                    value={answer.text}
-                    onChange={() => onClick(question.id, answer.id.toString())} // Convert answerId to string
-                    className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-                  />
-                  <span className="text-gray-800">{answer.text}</span>
-                </label>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <ul className="space-y-2">
+        {question.answers?.map((answer) => (
+          <li key={answer.id} className="ml-4">
+            <label className="flex items-center space-x-3">
+              <input
+                type="radio"
+                name={`question-${question.id}`}
+                value={answer.text}
+                onChange={() => onClick(question.id, answer.id.toString())} // Convert answerId to string
+                className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+              />
+              <span className="text-gray-800">{answer.text}</span>
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
