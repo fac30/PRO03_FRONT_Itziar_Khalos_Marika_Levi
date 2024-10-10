37eb60bf9a0b850c979c5b714e9b1eb56a64a9d3
Khalos Moscato <khalos.moscato@icloud.com>
2024-10-10 11:47:13 +0100

    removed h1

--- wisdom-war/src/Pages/HomePage.tsx
+++ wisdom-war/src/Pages/HomePage.tsx
@@ -6,7 +6,6 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Logo />
-      <h1 className="text-center text-2xl font-bold mt-4">Welcome to Wisdom War</h1>
       <div className="flex space-x-4 mt-8">
         <Button text="Create a new quiz" path="/create"/>
         <Button text="Explore our quizzes" path="/explore" />
